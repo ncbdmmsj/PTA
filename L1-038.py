222141afe6313e4d217e028e4f209e2326544898
Administrator <ujnwszikm@outlook.com>
2026-08-04 18:50:35 +0800

    L1-040 最佳情侣身高差

--- L1-038.py
+++ L1-038.py
@@ -1,2 +1,10 @@
-print("Hello World")
-print("Hello New World")
+x = input()
+x = int(x)
+for i in range(x):
+    c,y = input().split()
+    y = float(y)
+    if c =="F":
+        y = y*1.09
+    else :
+        y = y/1.09
+    print(f"{y:.2f}")
