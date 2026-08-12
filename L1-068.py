c94ece99f9d3c20233dd94d1325dc9382205cce3
Administrator <ujnwszikm@outlook.com>
2026-08-12 14:04:41 +0800

    L1-068 调和平均

--- L1-068.py
+++ L1-068.py
@@ -1,13 +1,14 @@
 n = int(input())
 a = list(map(float, input().split()))
 
-fenmu = 1.0
-for i in a:
-    fenmu *= i
+x = sum(1.0/x for x in a)
+# fenmu = 1.0
+# for i in a:
+#     fenmu *= i
+#
+# fenzi = 0.0
+# for i in a:
+#     fenzi += fenmu/i
 
-fenzi = 0.0
-for i in a:
-    fenzi += fenmu/i
-
-fenmu *= n
-print(f"{fenmu/fenzi: .2f}")
+# fenmu *= n
+print(f"{n/x:.2f}")
