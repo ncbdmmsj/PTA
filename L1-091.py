967a8ce7b303014ca77239efa2ba33cb2cebfe95
Administrator <ujnwszikm@outlook.com>
2026-08-13 21:10:56 +0800

    L1-091 程序员买包子

---
+++ L1-091.py
@@ -0,0 +1,11 @@
+n, x, m, k = input().split()
+n = int(n)
+m = int(m)
+k = int(k)
+
+if n == k:
+    print(f"mei you mai {x} de")
+elif k == m:
+    print(f"kan dao le mai {x} de")
+else:
+    print(f"wang le zhao mai {x} de")
